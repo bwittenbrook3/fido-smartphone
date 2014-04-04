941777442505057030dc1095d19b3fec30248187
Taylor Kelly <taylor.kelly@me.com>
2014-04-04 13:30:21 -0400

    Create new iOS Application

---
+++ FIDO K9/K9AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  K9AppDelegate.h
+//  FIDO K9
+//
+//  Created by Taylor on 4/4/14.
+//  Copyright (c) 2014 FIDO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface K9AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FIDO K9/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FIDO K9
+//
+//  Created by Taylor on 4/4/14.
+//  Copyright (c) 2014 FIDO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "K9AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([K9AppDelegate class]));
+    }
+}
