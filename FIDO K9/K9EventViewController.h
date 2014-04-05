ccc680bb1d460d72b1f59ef681232d6a60bce03a
Taylor Kelly <taylor.kelly@me.com>
2014-04-05 15:40:44 -0400

    Add Event View Controller

---
+++ FIDO K9/K9EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  K9EventViewController.h
+//  FIDO K9
+//
+//  Created by Taylor on 4/5/14.
+//  Copyright (c) 2014 FIDO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface K9EventViewController : UIViewController
+
+@end
---
+++ FIDO K9/K9EventViewController.m
@@ -0,0 +1,27 @@
+//
+//  K9EventViewController.m
+//  FIDO K9
+//
+//  Created by Taylor on 4/5/14.
+//  Copyright (c) 2014 FIDO. All rights reserved.
+//
+
+#import "K9EventViewController.h"
+
+@interface K9EventViewController ()
+
+@end
+
+@implementation K9EventViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
